8e6c7b18fddfd1c5a03ea26f5e7179973dca971b
DacFlorent <Dac.florent@gmail.com>
2025-05-21 23:45:51 +0200

    feat(G4-103): JavaFX moral bar view

--- src/main/java/com/fantasyhospital/view/CreatureCellView.java
+++ src/main/java/com/fantasyhospital/view/CreatureCellView.java
@@ -6,27 +6,43 @@ import javafx.scene.control.ListCell;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
+import javafx.geometry.Pos;
 
 import static com.fantasyhospital.util.CropImageUtils.cropImage;
 import static com.fantasyhospital.util.RemovePngBackgroundUtils.removePngBackground;
 
 public class CreatureCellView extends ListCell<Creature> {
-    private HBox content;
-    private ImageView imageView;
+    private VBox content;
+    private HBox topRow;
+    private ImageView creatureImageView;
+    private ImageView moraleImageView;
     private Text name;
     private Label detailsLabel;
 
     public CreatureCellView() {
         super();
-        imageView = new ImageView();
-        imageView.setFitHeight(30);
-        imageView.setFitWidth(30);
+        creatureImageView = new ImageView();
+        creatureImageView.setFitHeight(30);
+        creatureImageView.setFitWidth(30);
+
+        moraleImageView = new ImageView();
+        moraleImageView.setFitHeight(10);
+        moraleImageView.setFitWidth(65);
+
         name = new Text();
         detailsLabel = new Label();
         detailsLabel.setWrapText(true);
         detailsLabel.setStyle("-fx-text-fill: lightgray;");
-        content = new HBox(10, imageView, detailsLabel);
+
+        VBox imageAndBar = new VBox(3, creatureImageView, moraleImageView);
+        imageAndBar.setAlignment(Pos.CENTER);
+
+        topRow = new HBox(10, imageAndBar, name);
+        topRow.setAlignment(Pos.CENTER_LEFT);
+
+        content = new VBox(5, topRow, detailsLabel);
     }
 
     @Override
@@ -37,14 +53,44 @@ public class CreatureCellView extends ListCell<Creature> {
         } else {
             String imagePath = "/images/" + creature.getRace().toLowerCase() + ".png";
             Image image = new Image(getClass().getResourceAsStream(imagePath));
-
             Image transparentImage = removePngBackground(image);
             Image croppedImage = cropImage(transparentImage);
 
-            imageView.setImage(croppedImage);
+            creatureImageView.setImage(croppedImage);
+
+            moraleImageView.setImage(getMoraleImage(creature.getMorale()));
+
             name.setText(creature.getFullName());
             detailsLabel.setText(creature.toString());
             setGraphic(content);
+
         }
     }
+
+    private Image getMoraleImage(int morale) {
+        String moraleImagePath;
+
+        if (morale == 0) {
+            moraleImagePath = "/images/morale/EmptyLifeBar.png";
+        } else if (morale <= 10) {
+            moraleImagePath = "/images/morale/LowLifeBar1.png";
+        } else if (morale <= 20) {
+            moraleImagePath = "/images/morale/LowLifeBar2.png";
+        } else if (morale <= 30) {
+            moraleImagePath = "/images/morale/LowLifeBar3.png";
+        } else if (morale <= 40) {
+            moraleImagePath = "/images/morale/MediumLifeBar1.png";
+        } else if (morale <= 50) {
+            moraleImagePath = "/images/morale/MediumLifeBar2.png";
+        } else if (morale <= 70) {
+            moraleImagePath = "/images/morale/HighLifeBar1.png";
+        } else if (morale <= 80) {
+            moraleImagePath = "/images/morale/HighLifeBar2.png";
+        } else {
+            moraleImagePath = "/images/morale/FullLifeBar.png";
+        }
+
+        return new Image(getClass().getResourceAsStream(moraleImagePath));
+    }
 }
+
