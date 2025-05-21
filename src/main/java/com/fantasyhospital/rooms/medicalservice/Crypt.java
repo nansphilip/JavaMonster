d9ba1af4b4ef8131f62e7850319bedf6dfb5391f
Théo Chevallier <theolemayne@gmail.com>
2025-05-21 16:46:44 +0200

    feat: début implémentation crypte

--- src/main/java/com/fantasyhospital/rooms/medicalservice/Crypt.java
+++ src/main/java/com/fantasyhospital/rooms/medicalservice/Crypt.java
@@ -1,15 +1,61 @@
 package com.fantasyhospital.rooms.medicalservice;
 
-public class Crypt extends MedicalService {
+import com.fantasyhospital.model.creatures.abstractclass.Creature;
+import com.fantasyhospital.model.creatures.interfaces.Regenerating;
 
-    private final int ventilation;
-    private final double temperature;
+import java.util.HashMap;
+import java.util.Random;
 
-    public Crypt(String name, double area, int MAX_CAPACITY, String budget, int ventilation, double temperature) {
+public final class Crypt extends MedicalService {
+
+    private boolean airflow;
+    private int temperature;
+    private HashMap<Creature, Integer> nbToursCreatureWaits = new HashMap<>();
+
+    public Crypt(String name, double area, int MAX_CAPACITY, String budget) {
         super(name, area, MAX_CAPACITY, budget);
-        this.ventilation = ventilation;
-        this.temperature = temperature;
+        this.airflow = true;
+        this.temperature = 20;
     }
 
-    // Getters et setters omis pour la clarté
+    /**
+     * Adds a creature to the crypte if the capacity and interface (regenerating) are compatible.
+     *
+     * @param creature the creature to add to the service
+     */
+    @Override
+    public boolean addCreature(Creature creature) {
+        if(Regenerating.class.isAssignableFrom(creature.getClass())) {
+            if (creatures.size() >= MAX_CREATURE) {
+                return false;
+            }
+            this.creatures.add(creature);
+            //return super.addCreature(creature);
+        }
+        return false;
+    }
+
+    public void airflowBreakDown(){
+        this.airflow = false;
+    }
+
+    public void airflowRepair(){
+        this.airflow = true;
+    }
+
+    public void updateTemperature(){
+        if(this.airflow && this.temperature > 20){
+            this.temperature = Math.max(20, this.temperature - 10);
+        } else {
+            this.temperature += 2 + new Random().nextInt(10);
+        }
+    }
+
+    public void manageAirFlow(){
+        if(this.airflow && Math.random() < 0.35){
+            airflowBreakDown();
+        } else if(!this.airflow && Math.random() < 0.20){
+            airflowRepair();
+        }
+    }
 }
